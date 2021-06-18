3a5f5e96370b9fc84809af48184cc708c297ea03
Antonio Gutierrez <nisba.rog@gmail.com>
2021-06-18 05:52:16 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const productName = document.querySelector("#productName");
+const productPrice = document.querySelector("#productPrice");
+const buttonSave = document.querySelector("#button-save");
+const buttonCancel = document.querySelector("#button-cancel");
+const productList = document.querySelector("#product-list");
+const totalOutput = document.querySelector("#total");
+
+let = total = 0;
+
+const createNewProduct = (name, price) => {
+  const ionCard = document.createElement("ion-card");
+  const newProductItem = document.createElement("ion-card-content");
+  ionCard.appendChild(newProductItem);
+  newProductItem.textContent = name + " : S/" + price;
+  productList.appendChild(ionCard);
+};
+
+const clearInputs = () => {
+  productName.value = "";
+  productPrice.value = "";
+};
+
+const presentAlert = () => {
+  const alert = document.createElement("ion-alert");
+  alert.header = "calichin la info estan mal";
+  alert.subHeader = "chequea bien pe";
+  alert.message = "Te webeaste en el precio o el nombre";
+  alert.buttons = ["Picale aqui perro"];
+
+  document.body.appendChild(alert);
+  return alert.present();
+};
+
+const isEmpty = (str) => !str.trim().length;
+
+buttonSave.addEventListener("click", () => {
+  const name = productName.value;
+  const price = productPrice.value;
+  if (isEmpty(name) || price <= 0 || isEmpty(price)) {
+    presentAlert();
+    // console.log("no valid data");
+    return;
+  }
+
+  // console.log(name,price);
+  createNewProduct(name, price);
+  total += +price;
+  totalOutput.textContent = total;
+
+  clearInputs();
+});
+
+buttonCancel.addEventListener("click", clearInputs);
